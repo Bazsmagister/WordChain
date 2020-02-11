e405f3ebaa9b8b3c157f537cb5b7c10d3e79f7be
Bazsmagister <bosmer007@gmail.com>
2020-02-11 18:01:41 +0100

    @include

---
+++ resources/views/header.blade.php
@@ -0,0 +1,11 @@
+<div id="header" class="container">
+
+    <div id="menu">
+        <ul>
+            <li class="current_page_item"><a href="#" accesskey="1" title="">Homepage</a></li>
+            <li><a href="#" accesskey="2" title="">Rules</a></li>
+            <li><a href="#" accesskey="3" title="">About Us</a></li>
+            <li><a href="#" accesskey="4" title="">Contact Us</a></li>
+        </ul>
+    </div>
+</div>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -31,18 +31,12 @@ Released   : 20140225
 </head>
 
 <body>
+
+@yield ('header')
+
+
     <div id="header-wrapper">
-        <div id="header" class="container">
-            
-            <div id="menu">
-                <ul>
-                    <li class="current_page_item"><a href="#" accesskey="1" title="">Homepage</a></li>
-                    <li><a href="#" accesskey="2" title="">Rules</a></li>
-                    <li><a href="#" accesskey="3" title="">About Us</a></li>
-                    <li><a href="#" accesskey="4" title="">Contact Us</a></li>
-                </ul>
-            </div>
-        </div>
+        
         <div id="header-featured">
             <div id="banner-wrapper">
                 <div id="banner" class="container">
